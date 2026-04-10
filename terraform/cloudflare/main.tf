fbe3e1797160e3c31b4f037a7fddf6767f280a9d
Luis Amaral <luis.c.amaral@gmail.com>
2026-04-09 23:50:02 -0600

    feat(portal): add login.cf.lcamaral.com DNS + tunnel ingress

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -50,6 +50,16 @@ resource "cloudflare_dns_record" "auth_cf_tunnel" {
   ttl     = 1
 }
 
+# Homelab Portal: login.cf.lcamaral.com -> tunnel
+resource "cloudflare_dns_record" "login_cf_tunnel" {
+  zone_id = cloudflare_zone.lcamaral_com.id
+  type    = "CNAME"
+  name    = "login.cf.lcamaral.com"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.bologna.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
 # MinIO S3 API: s3.cf.lcamaral.com -> tunnel
 resource "cloudflare_dns_record" "s3_cf_tunnel" {
   zone_id = cloudflare_zone.lcamaral_com.id
@@ -128,6 +138,13 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "bologna" {
     ingress = [
       {
         hostname = "auth.cf.lcamaral.com"
+        service  = "https://nginx-rproxy:443"
+        origin_request = {
+          no_tls_verify = true
+        }
+      },
+      {
+        hostname = "login.cf.lcamaral.com"
         service  = "https://nginx-rproxy:443"
         origin_request = {
           no_tls_verify = true
